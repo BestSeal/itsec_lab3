a1809146c6b83acdbbcf3f22af5e69d5c6f62a98
BestSeal <thatlazyseal@gmail.com>
2021-05-16 21:40:24 +0300

    Done

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+
+namespace itsec_lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Steganograph stg = new Steganograph();
+            File.WriteAllBytes(@"D:\txt.txt",stg.GetInfoFromBmp(@"D:\1.bmp").ToArray());
+            stg.HideTxtIntoBmp(@"D:\1_1.bmp", @"D:\wop.txt");
+            File.WriteAllBytes(@"D:\1wop.txt",stg.GetInfoFromBmp(@"D:\1_1.bmp").ToArray());
+        }
+    }
+}
